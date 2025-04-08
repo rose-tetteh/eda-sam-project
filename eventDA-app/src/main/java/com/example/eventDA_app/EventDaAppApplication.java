84789a285b8bc16bfd2661bd5b5c96517c93a0fd
rose-tetteh <rose.tetteh@amalitech.com>
2025-04-08 09:56:34 +0000

    feat: libraries and config files

---
+++ eventDA-app/src/main/java/com/example/eventDA_app/EventDaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventDA_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDaAppApplication.class, args);
+	}
+
+}
---
+++ eventDA-app/src/test/java/com/example/eventDA_app/EventDaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventDA_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
